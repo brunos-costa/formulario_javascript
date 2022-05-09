b2add4592e2fa1584a9b01fdf8508d0dc8d528b0
Bruno Costa <abrunoscosta@gmail.com>
2022-05-09 18:50:51 -0300

    Iniciado formulário com Javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//alert("Olá Mundo!")
+/*console.error("Se você é front-ender, vai me encontrar!")*/
+
+let nome = document.querySelector("#nome")
+nome.addEventListener('keyup',validarCampo)
+
+function validarCampo(){
+    //console.log(nome.value)
+    if(nome.value == ""){
+        nome.classList.remove("border-success")
+        nome.classList.add("border", "border-danger")
+    }
+    else{
+        nome.classList.remove("border-danger")
+        nome.classList.add("border", "border-success")
+    }
+}
+
+/* INSERIR ENDEREÇO EM CAMPO SOMENTE LEITURA */
+let endereco = document.querySelector("#endereco")
+let leitura = document.querySelector("input[readonly]")
+
+endereco.addEventListener("keyup",function(){
+    leitura.value = endereco.value
+})
